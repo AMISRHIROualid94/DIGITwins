ba2ba22a44bde923496cf5188e92742e7b104cbf
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-02-21 20:14:57 +0100

    Add Eureka Client

--- Employer-Service/src/main/java/com/sf/digitwins/EmployerServiceApplication.java
+++ Employer-Service/src/main/java/com/sf/digitwins/EmployerServiceApplication.java
@@ -2,8 +2,10 @@ package com.sf.digitwins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EmployerServiceApplication {
 
     public static void main(String[] args) {
--- Materiel-Service/src/main/java/com/sf/digitwins/MaterielServiceApplication.java
+++ Materiel-Service/src/main/java/com/sf/digitwins/MaterielServiceApplication.java
@@ -2,8 +2,10 @@ package com.sf.digitwins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MaterielServiceApplication {
 
     public static void main(String[] args) {
