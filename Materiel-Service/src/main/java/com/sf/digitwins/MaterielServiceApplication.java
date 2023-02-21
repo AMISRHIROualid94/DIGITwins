7102b36ceeed32f4c9ab808417f65859d8c98d75
AMISRHIROualid94 <56658551+AMISRHIROualid94@users.noreply.github.com>
2023-02-21 19:28:33 +0100

    Add Eureka Server Module and add WebClient for MetrielService

--- Materiel-Service/src/main/java/com/sf/digitwins/MaterielServiceApplication.java
+++ Materiel-Service/src/main/java/com/sf/digitwins/MaterielServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockServiceApplication {
+public class MaterielServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockServiceApplication.class, args);
+        SpringApplication.run(MaterielServiceApplication.class, args);
     }
 
 }
